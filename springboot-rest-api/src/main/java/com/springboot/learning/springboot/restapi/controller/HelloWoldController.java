76c944f5d0db7e58643e58fc71a21f1bcfef17df
raju.paridi <paridiraju@gmail.com>
2023-10-12 10:00:16 +0530

    RestController annotation added

--- springboot-rest-api/src/main/java/com/springboot/learning/springboot/restapi/controller/HelloWoldController.java
+++ springboot-rest-api/src/main/java/com/springboot/learning/springboot/restapi/controller/HelloWoldController.java
@@ -3,9 +3,9 @@ package com.springboot.learning.springboot.restapi.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@ResponseBody
+@RestController
 public class HelloWoldController {
 
     // Get request
